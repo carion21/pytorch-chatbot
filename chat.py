f41daed4d275762316e4b93451d4e33dcc11fafb
carion21 <medimickael99@gmail.com>
2024-10-08 16:30:20 +0000

    show device

--- chat.py
+++ chat.py
@@ -8,6 +8,8 @@ from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+print('device:', device)
+
 with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
